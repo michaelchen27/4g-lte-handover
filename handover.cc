ff2c8327af6fa0fb9d53ce499d0b86b5d9c7a57b
michaelchen27 <michael.chen2701@gmail.com>
2021-11-21 16:00:06 +0700

    init commit

---
+++ handover.cc
@@ -0,0 +1,12 @@
+#include <ns3/core-module.h>
+#include <ns3/network-module.h>
+#include <ns3/mobility-module.h>
+#include <ns3/lte-module.h>
+
+using namespace ns3;
+
+int main (int argc, char *argv[]) {
+
+	//LTE Helper Object
+	Ptr<LteHelper> lteHelper = CreateObject<LteHelper> ();
+}
