c0ae90a8090c64823d926badc6a80c5da2861e87
michaelchen27 <michael.chen2701@gmail.com>
2021-11-22 14:22:14 +0700

    Setup EPC error

--- handover.cc
+++ handover.cc
@@ -2,25 +2,46 @@
 #include <ns3/network-module.h>
 #include <ns3/mobility-module.h>
 #include <ns3/lte-module.h>
+#include <ns3/config-store.h>
+#include <ns3/netanim-module.h>
 
 using namespace ns3;
 
 int main (int argc, char *argv[]) {
 
 	// LTE Helper Object
-	// Instantiate common objects and provide methods to add eNBs and UEs and configure them.
-	
+	// Instantiate common objects and provide methods to add eNBs and UEs and configure them.	
 	Ptr<LteHelper> lteHelper = CreateObject<LteHelper> ();
+	
+	//Configure Scheduler and Handover algo
+	lteHelper->SetSchedulerType ("ns3::PssFfMacScheduler");    // PSS scheduler
+	lteHelper->SetHandoverAlgorithmType ("ns3::A2A4RsrqHandoverAlgorithm");
+	lteHelper->SetHandoverAlgorithmAttribute ("ServingCellThreshold", UintegerValue (30));
+	lteHelper->SetHandoverAlgorithmAttribute ("NeighbourCellOffset", UintegerValue (1));
+
+
+	// Create Evolved Packet Core (EPC), use the EPC Helper class to take care of creating the EPC entities and network.
+	Ptr<PointToPointEpcHelper> epcHelper = CreateObject<PointToPointEpcHelper>();
+
+
+	// Tell LTE helper to use the EPC
+	//lteHelper->SetEpcHelper(epcHelper); //TODO: This code still produces ERROR
+	
+
+	//Setup PGW
+	Ptr<Node> pgw = epcHelper->GetPgwNode ();
+ 
 
 	// Empty nodes without LTE Protocol stack
-	
-	// Create Node objects for eNBs
+	// Create Node objects for eNBs, create 3 eNB Nodes.
 	NodeContainer enbNodes;
-	enbNodes.Create (1);
+	enbNodes.Create (3);
 	
-	// Create Node objects for UEs
+
+	// Create Node objects for UEs, create 4 user nodes.
 	NodeContainer ueNodes;
-	ueNodes.Create (2);
+	ueNodes.Create (4);
+
 
 	// Mobility model for all nodes
 	// Place all nodes at (0,0,0) coordinates.
@@ -29,6 +50,7 @@ int main (int argc, char *argv[]) {
 	mobility.Install (enbNodes);
 	mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");	
 	mobility.Install (ueNodes);
+
 
 	// Install LTE protocol stack on eNBs
 	NetDeviceContainer enbDevs;
@@ -49,6 +71,10 @@ int main (int argc, char *argv[]) {
 
 	// Set the stop time, otherwise it will run forever.
 	Simulator::Stop (Seconds(0.005));
+
+	//Setup NetAnim
+	AnimationInterface anim("handover.xml");
+	anim.SetConstantPosition(pgw, 91.0, 20.0, 20.0);
 
 	Simulator::Run();
 
